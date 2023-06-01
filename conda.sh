86d54859a815d46af06542c46bf6154e9aa1c1cd
John Doe <johndoe@email.com>
2023-06-01 03:27:38 +0000

    lala

--- conda.sh
+++ conda.sh
@@ -5,13 +5,3 @@ rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 ~/miniconda3/bin/conda init zsh
 
-## Create a new environment from scratch
-conda create --prefix ./.venv
-
-# Tratar de instalar esto primero
-conda install -c pytorch pytorch-cuda
-
-conda install -c conda-forge transformers=4.29.0
-
-conda install -c anaconda ipykernel
-
--- poetry.sh
+++
@@ -1,6 +0,0 @@
-curl -sSL https://install.python-poetry.org | python3 -
-export PATH="/root/.local/bin:$PATH"
-
-poetry config --local virtualenvs.in-project true
-poetry init
-poetry add ipykernel
